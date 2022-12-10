21dc556e0a52a32aa0626a8fd0116762f8075a27
ErhanEce <erhanecee62@gmail.com>
2022-12-10 23:46:45 +0300

    Odev8

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var ctrlMekanlar = require('../controllers/mekanlar.js')
 var ctrlDigerleri = require('../controllers/digerleri.js')
 
-/* GET home page. */
 router.get('/', ctrlMekanlar.anaSayfa);
 router.get('/mekan/:mekanid', ctrlMekanlar.mekanBilgisi);
 router.get('/mekan/:mekanid/yorum/yeni',ctrlMekanlar.yorumEkle);
